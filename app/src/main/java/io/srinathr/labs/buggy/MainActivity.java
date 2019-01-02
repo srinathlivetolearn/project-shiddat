17e8341e9213ea8abbb14f3060bfe2302a6a0223
Srinath-R <r.srinath@live.com>
2019-01-02 21:38:49 +0530

    added list view

--- app/src/main/java/io/srinathr/labs/buggy/MainActivity.java
+++ app/src/main/java/io/srinathr/labs/buggy/MainActivity.java
@@ -1,10 +1,25 @@
 package io.srinathr.labs.buggy;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    protected static final String[] PROJECTION = {ContactsContract.Data._ID,ContactsContract.Data.DISPLAY_NAME};
+
+    protected static final String SELECTION = "((" +
+            ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
+            ContactsContract.Data.DISPLAY_NAME + " != '' ))";
+
+    SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +30,21 @@ public class MainActivity extends AppCompatActivity {
     public void searchContacts(View view) {
 
     }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new CursorLoader(this,ContactsContract.Data.CONTENT_URI,PROJECTION,SELECTION,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
 }
